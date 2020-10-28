2e763b00feb69eee83b67e86f87da5ce594ff5a3
Lasse M <lasse.mollerup@typeform.com>
2020-10-28 21:48:44 +0100

    the Google Chrome console, keeps saying my arrays are undefined, seems to be something to do with Lazy Load on console.log(). Will try to insert the Dino data directly in my file

--- app.js
+++ app.js
@@ -3,19 +3,23 @@ var allDinos = [];
 
 // Fetching data from the dino.json file in repository
 
+
+
 const getDataFromJson = (async() => {
 
     // Attempt to retreive data from the json file:
     try{
         let response = await (await fetch('./dino.json')).json();
 
+        // console.log('response.Dinos =>', response.Dinos)
 
-        allDinos.push(response.Dinos)
+        response.Dinos.forEach(ele => {
+            // console.log(ele)
+            // console.log(response.Dinos[ele])
 
-        //If all goes well, response is stored
-        return {
-            allDinos
-        }
+            allDinos.push(ele)
+        })
+        
     }   // If error happens, the following is show:
         catch (error) {
         console.log('Error in try/catch=>', error)
@@ -23,9 +27,42 @@ const getDataFromJson = (async() => {
 
 })();
 
-console.log('allDinos =>', allDinos)
-    // Create Dino Constructor
+// console.log('allDinos', allDinos);
+// console.log('allDinos =>', allDinos);
 
+    // (function newLoop(arrnew) {
+    //     debugger
+    //     // console.log(arrnew);
+
+    //    arrnew.forEach(ele => console.log(ele));
+
+  
+             
+    // })(allDinos);
+
+    // const newLoop = function(arrnew) {
+    //     // debugger
+    //     // console.log(arrnew);
+
+    //    arrnew.forEach(ele => console.log(ele));
+    // };
+
+    let myLoopFunction = (function(arr) {
+        temp = arr
+        let arrnew = []
+        function innerLoop (temp) {
+            temp.forEach( (ele, idx) => arrnew.push(ele[idx]));
+            console.log(arrnew);
+        }
+        return{
+            newMethod: innerLoop
+        };
+    }());
+
+   
+    
+
+    // Create Dino Constructor
 
     // Create Dino Objects
 
@@ -48,10 +85,23 @@ console.log('allDinos =>', allDinos)
 
 
     // Generate Tiles for each Dino in Array
-  
+    (function ToggleForm(params) {
+        var elem = document.getElementById('grid');
+        elem.style['grid-template-columns'] = 'repeat(3, 1fr';
+        elem.style['grid-template-rows'] = 'repeat(3, 1fr';
+        elem.style['grid-template-rows'] = 'repeat(3, 1fr';
+        elem.style['grid-row-gap'] = '3px';
+        elem.style['grid-column-gap'] = '3px';
+        elem.style['width'] = '80vw';
+        elem.style['height'] = '80vh';
+        elem.style['border'] = '1px black solid';
+    })(); 
         // Add tiles to DOM
 
     // Remove form from screen
-
+    (function ToggleForm(params) {
+        var elem = document.getElementById('dino-compare');
+        elem.style.display = 'none';
+    })();
 
 // On button click, prepare and display infographic
